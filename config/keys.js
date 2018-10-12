b155a92240c71ed4e355dfc3736639d551dec5d0
SatyaJuttiga <40733094+SatyaJuttiga@users.noreply.github.com>
2018-10-12 15:49:14 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
     google:{
-        clientID:'894426750519-a936tvgk852461c3hrbqf921v5t4j2hn.apps.googleusercontent.com',
-        clientSecret:'g_Dj6Ztj7EbRjXOARJn0HI-b'
+        clientID:'**********************',
+        clientSecret:'***************'
     },
     mongodb:{
-        dbURI:'mongodb://auth:auth%402@ds111113.mlab.com:11113/capstone'
+        dbURI:'mongodb://****:*******@ds111113.mlab.com:11113/capstone'
     },
     session:{
         cookieKey:'awesome project'
